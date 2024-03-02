99466529bc238f0ef20a15f2b8dfb812f08acd6d
Heciar <franheciar@gmail.com>
2024-03-02 19:20:54 +0100

    Prueba consulta

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,26 @@ router.get('/connect', async (req, res) =>{
 res.json(result[0])
 });
 
+router.post('/auth', async (req, res) => {
+    const { username, password } = req.body;
+    const isOk = await isAuth(username, password);
+    console.log(isOk);
+
+    if (!username || !password || !isOk) {
+      let errorMessage = "¡ESCRIBA UN USUARIO Y SU CONTRASEÑA!";
+      if (isOk) errorMessage = "USUARIO y/o PASSWORD incorrectas";
+
+      res.status(400).send(errorMessage);
+    } else {
+      res.status(200).send("¡LOGIN CORRECTO!");
+    }
+  });
+
 router.post('/insertUsers', insertUsers)
+
+const isAuth = (username, password) => {
+    const res = pool.query('SELECT * FROM users WHERE nameuser=\'' + username + '\' AND pswd=\'' + password +'\'');
+    return res;
+}   
 
 export default router;
